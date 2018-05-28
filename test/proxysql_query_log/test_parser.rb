abb4d0938537c275a98ba746cdbf2156789fd735
r_takaishi <ryo.takaishi.0@gmail.com>
2018-05-28 22:58:20 +0900

    update

--- test/proxysql_query_log/test_parser.rb
+++ test/proxysql_query_log/test_parser.rb
@@ -2,33 +2,7 @@ require 'test/unit'
 require 'proxysql_query_log/parser'
 
 class TestSample < Test::Unit::TestCase
-  class << self
-    # テスト群の実行前に呼ばれる．変な初期化トリックがいらなくなる
-    def startup
-      p :_startup
-    end
-
-    # テスト群の実行後に呼ばれる
-    def shutdown
-      p :_shutdown
-    end
-  end
-
   TMP_DIR = File.dirname(__FILE__) + '/../tmp/proxysql_query_log'
-  # 毎回テスト実行前に呼ばれる
-  def setup
-    p :setup
-  end
-
-  # テストがpassedになっている場合に，テスト実行後に呼ばれる．テスト後の状態確認とかに使える
-  def cleanup
-    p :cleanup
-  end
-
-  # 毎回テスト実行後に呼ばれる
-  def teardown
-    p :treadown
-  end
 
   def test_singlefile
     File.open("#{TMP_DIR}/query_log", 'w+b') do |f|
@@ -38,8 +12,6 @@ class TestSample < Test::Unit::TestCase
       parser = ProxysqlQueryLog::Parser.new
       q = parser.read(f)[0]
 
-      # assert_equal(true, events.length > 0)
-      # assert_equal(1, 1)
       assert_equal(9, q.thread_id)
       assert_equal('root', q.username)
       assert_equal('alpaca', q.schema_name)
